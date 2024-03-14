a52c5bf94f165995d9d212017748c30724890bf3
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-15 00:10:57 +0100

    repository

--- src/datas/models/Entry.java
+++ src/datas/models/Entry.java
@@ -1,4 +1,42 @@
 package datas.models;
 
+import java.time.LocalDate;
+
 public class Entry {
+
+    private final int id;
+    private String title;
+    private String body;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public LocalDate getDateCreated() {
+        return dateCreated;
+    }
+
+    private final LocalDate dateCreated = LocalDate.now();
+
+    public Entry(int id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
 }
