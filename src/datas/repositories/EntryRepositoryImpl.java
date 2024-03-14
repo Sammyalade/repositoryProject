5483260ededbe92563c09133f064549b6127b0d0
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-14 08:48:30 +0100

    repository

---
+++ src/datas/repositories/EntryRepositoryImpl.java
@@ -0,0 +1,37 @@
+package datas.repositories;
+
+import datas.models.Entry;
+
+import java.util.List;
+
+public class EntryRepositoryImpl implements EntryRepository{
+    @Override
+    public Entry save(Entry entry) {
+        return null;
+    }
+
+    @Override
+    public List<Entry> findAll() {
+        return null;
+    }
+
+    @Override
+    public Entry findById(int id) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public void delete(Entry entry) {
+
+    }
+}
