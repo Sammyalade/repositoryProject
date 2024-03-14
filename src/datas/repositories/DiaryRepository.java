c47a645ed6c59d0e82f9b7a55dd0f63039d3fd7d
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-14 08:46:51 +0100

    repository

---
+++ src/datas/repositories/DiaryRepository.java
@@ -0,0 +1,15 @@
+package datas.repositories;
+
+import datas.models.Diary;
+
+import java.util.List;
+
+public interface DiaryRepository {
+
+    Diary save(Diary diary);
+    List<Diary> findAll();
+    Diary findById(String username);
+    long count();
+    void delete(String username);
+    void delete(Diary diary);
+}
