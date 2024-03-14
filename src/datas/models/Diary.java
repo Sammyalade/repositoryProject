618b15e9de59c02dea54f8db373138d9603634cb
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-14 23:49:50 +0100

    repository

--- src/datas/models/Diary.java
+++ src/datas/models/Diary.java
@@ -1,4 +1,48 @@
 package datas.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Diary {
+
+    private String username;
+    private String password;
+    public boolean isLocked = true;
+
+    public Diary(){}
+
+    public Diary(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isLocked() {
+        return isLocked;
+    }
+
+    public void setLocked(boolean locked) {
+        isLocked = locked;
+    }
+
+    public List<Entry> getEntries() {
+        return entries;
+    }
+
+    private final List<Entry> entries = new ArrayList<>();
 }
