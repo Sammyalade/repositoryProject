49439dd64a105b6b8eb7740c882124dc597e0c62
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-17 11:08:20 +0100

    repository

--- test/services/EntryServiceTest.java
+++ test/services/EntryServiceTest.java
@@ -1,7 +1,23 @@
 package services;
 
+import datas.repositories.EntryRepository;
+import datas.repositories.EntryRepositoryImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class EntryServiceTest {
 
+    private EntryService entryService;
+
+    @BeforeEach
+    public void initializeEntryService(){
+        EntryRepository entryRepository = new EntryRepositoryImpl();
+        entryService = new EntryServiceImpl(entryRepository);
+    }
+
+    @Test
+    public void createEntry_returnsEntryTest(){
+
+        entryService.create()
+    }
 }
