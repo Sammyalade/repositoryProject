78f5bc33e642e407d3a7519ce44143b6d441b784
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-17 11:41:48 +0100

    repository

--- src/services/dtos/EntryCreationRequest.java
+++ src/services/dtos/EntryCreationRequest.java
@@ -1,4 +1,23 @@
 package services.dtos;
 
 public class EntryCreationRequest {
+
+
+    public String getTitle() {
+        return null;
+    }
+
+    public String getBody() {
+        return null;
+    }
+
+    public void setTitle(String firstEntry) {
+    }
+
+    public void setBody(String thisIsMyFirstEntry) {
+    }
+
+    public int getId() {
+        return 0;
+    }
 }
