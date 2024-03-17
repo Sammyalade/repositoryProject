d11a3eac4a67b7b83e6285a1e62a6527c95f68ba
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-17 23:22:19 +0100

    repository

--- test/services/EntryServiceTest.java
+++ test/services/EntryServiceTest.java
@@ -8,6 +8,9 @@ import org.junit.jupiter.api.Test;
 import services.dtos.EntryCreationRequest;
 import services.dtos.EntryUpdateRequest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EntryServiceTest {
@@ -49,5 +52,36 @@ public class EntryServiceTest {
         entryCreationRequest.setBody("This is my first entry");
         entryService.create(entryCreationRequest);
         EntryUpdateRequest entryUpdateRequest = new EntryUpdateRequest();
+        entryUpdateRequest.setId(1);
+        entryUpdateRequest.setTitle("Updated Entry");
+        entryUpdateRequest.setBody("This is my updated entry");
+        Entry entryToUpdate = entryService.update(entryUpdateRequest);
+        assertEquals("Updated Entry", entryToUpdate.getTitle());
     }
+
+    @Test
+    public void createTwoEntries_getAllEntries_returnAListOfEntries(){
+        EntryCreationRequest entryCreationRequest = new EntryCreationRequest();
+        entryCreationRequest.setTitle("First Entry");
+        entryCreationRequest.setBody("This is my first entry");
+        EntryCreationRequest entryCreationRequest2 = new EntryCreationRequest();
+        entryCreationRequest2.setTitle("First Entry");
+        entryCreationRequest2.setBody("This is my first entry");
+        Entry entry1 = entryService.create(entryCreationRequest);
+        Entry entry2 = entryService.create(entryCreationRequest2);
+        List<Entry> entryList = List.of(new Entry[]{entry1, entry2});
+        assertEquals(entryList, entryService.getAllEntries());
+    }
+
+    @Test
+    public void createEntry_findEntryById_returnsEntryTest(){
+        EntryCreationRequest entryCreationRequest = new EntryCreationRequest();
+        entryCreationRequest.setTitle("First Entry");
+        entryCreationRequest.setBody("This is my first entry");
+        Entry entry = entryService.create(entryCreationRequest);
+        assertEquals(entry, entryService.checkEntryById(1));
+    }
+
+    @Test
+    public void
 }
