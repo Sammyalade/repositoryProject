17196b3fb678cb002e4eb4e32f648d4a2e634e60
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-17 23:21:45 +0100

    repository

--- src/services/dtos/EntryUpdateRequest.java
+++ src/services/dtos/EntryUpdateRequest.java
@@ -1,4 +1,32 @@
 package services.dtos;
 
 public class EntryUpdateRequest {
+
+    private long id;
+    private String title;
+    private String body;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public long getId() {
+        return id;
+    }
 }
