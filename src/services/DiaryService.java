196f8f3d48e9fac6a5f0e87ca03ac88a816c29dd
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-17 09:15:06 +0100

    repository

--- src/services/DiaryService.java
+++ src/services/DiaryService.java
@@ -7,8 +7,4 @@ import services.dtos.RegisterRequest;
 public interface DiaryService {
     Diary register(RegisterRequest registerRequest);
     boolean login(LoginRequest loginRequest);
-
-    void createEntry();
-
-    void createEntry(LoginRequest loginRequest,);
 }
