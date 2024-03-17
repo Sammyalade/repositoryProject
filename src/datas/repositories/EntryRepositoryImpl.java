3d892d5afd166cf44310b969ca7001365fa62405
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-17 09:12:38 +0100

    repository

--- src/datas/repositories/EntryRepositoryImpl.java
+++ src/datas/repositories/EntryRepositoryImpl.java
@@ -2,36 +2,48 @@ package datas.repositories;
 
 import datas.models.Entry;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class EntryRepositoryImpl implements EntryRepository{
+    private long count;
+    private final List<Entry> entries = new ArrayList<>();
+
     @Override
     public Entry save(Entry entry) {
+        entries.add(entry);
+        count++;
         return null;
     }
 
     @Override
     public List<Entry> findAll() {
-        return null;
+        if(entries.isEmpty()) return null;
+        return entries;
     }
 
     @Override
     public Entry findById(int id) {
+        for(Entry entry: entries) {
+            if (entry.getId() == id) return entry;
+        }
         return null;
     }
 
     @Override
     public long count() {
-        return 0;
+        return count;
     }
 
     @Override
     public void delete(int id) {
-
+        entries.remove(findById(id));
+        count--;
     }
 
     @Override
     public void delete(Entry entry) {
-
+        entries.remove(entry);
+        count--;
     }
 }
