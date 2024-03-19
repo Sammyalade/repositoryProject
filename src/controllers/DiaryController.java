0bde36cf44ed61f017ee55fce08988f569cbae78
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-19 19:50:58 +0100

    repository

--- src/controllers/DiaryController.java
+++ src/controllers/DiaryController.java
@@ -1,13 +1,25 @@
 package controllers;
 
+import datas.models.Entry;
 import services.DiaryService;
 import services.DiaryServiceImpl;
+import services.EntryService;
+import services.EntryServiceImpl;
+import services.dtos.LoginRequest;
+import services.dtos.RegisterRequest;
 
 public class DiaryController {
 
     private DiaryService diaryService = new DiaryServiceImpl();
+    private EntryService entryService = new EntryServiceImpl();
 
-    public void registerUser(){
-        
+    public void registerUser(RegisterRequest registerRequest){
+        diaryService.register(registerRequest);
     }
+
+    public void login(LoginRequest loginRequest){
+        diaryService.login(loginRequest);
+    }
+
+    public void
 }
