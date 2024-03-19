c41bd0aec8d5545c93e6b82d6d730116477e2568
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-19 04:02:06 +0100

    repository

--- src/services/EntryService.java
+++ src/services/EntryService.java
@@ -10,7 +10,7 @@ public interface EntryService {
 
     Entry create(EntryCreationRequest newEntry);
 
-    List<Entry> getAllEntries();
+    List<Entry> getAllEntries(String username);
 
     Entry checkEntryById(int id);
 
