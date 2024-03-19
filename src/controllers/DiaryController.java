240bd8a6f9deec24b5d4e3ec28c4f546310de0d6
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-19 19:55:28 +0100

    repository

--- src/controllers/DiaryController.java
+++ src/controllers/DiaryController.java
@@ -5,8 +5,11 @@ import services.DiaryService;
 import services.DiaryServiceImpl;
 import services.EntryService;
 import services.EntryServiceImpl;
+import services.dtos.EntryCreationRequest;
 import services.dtos.LoginRequest;
 import services.dtos.RegisterRequest;
+
+import java.util.List;
 
 public class DiaryController {
 
@@ -19,6 +22,22 @@ public class DiaryController {
 
     public void login(LoginRequest loginRequest){
         diaryService.login(loginRequest);
+    }
+
+    public void logout(String username){
+        diaryService.logout(username);
+    }
+
+    public void createEntry(EntryCreationRequest entryCreationRequest){
+        diaryService.createEntry(entryCreationRequest);
+    }
+
+    public List<Entry> getAllEntriesBy(String username){
+       return entryService.getAllEntries(username);
+    }
+
+    public void deleteEntry(int id){
+        entryService.delete(id);
     }
 
     public void
