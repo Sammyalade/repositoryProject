8362ace32fc14f4ee3c28a9e0b7cf36a1062e1bb
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-19 19:43:49 +0100

    repository

---
+++ src/controllers/DiaryController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import services.DiaryService;
+import services.DiaryServiceImpl;
+
+public class DiaryController {
+
+    private DiaryService diaryService = new DiaryServiceImpl();
+
+    public void registerUser(){
+        
+    }
+}
