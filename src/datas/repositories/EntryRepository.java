a000dc82e3c428eb9fa579502d65233e7fd227bf
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-20 23:15:59 +0100

    repository

--- src/datas/repositories/EntryRepository.java
+++ src/datas/repositories/EntryRepository.java
@@ -12,7 +12,6 @@ public interface EntryRepository {
     long count();
     void delete(int id);
     void delete(Entry entry);
-
     void deleteAll();
     Entry findBy(String username);
 }
--- test/controller/DiaryControllerTest.java
+++ test/controller/DiaryControllerTest.java
@@ -1,9 +1,32 @@
 package controller;
 
+import controllers.DiaryController;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import services.DiaryService;
+import services.DiaryServiceImpl;
+import services.dtos.RegisterRequest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DiaryControllerTest {
 
-//    @Test
-//    public void
+    private DiaryController diaryController;
+    @BeforeEach
+    public void setUp(){
+        diaryController = new DiaryController();
+    }
+
+    @Test
+    public void RegisterUser_userIsRegisteredTest(){
+          RegisterRequest registerRequest = new RegisterRequest();
+          registerRequest.setUsername("username");
+          registerRequest.setPassword("password");
+          assertEquals("Registration Successful", diaryController.registerUser(registerRequest));
+      }
+
+
+    @Test
+    public void
+
 }
