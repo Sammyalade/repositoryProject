5252098e90f995ef5e6adc35cb286e58e546ddc3
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-21 10:29:26 +0100

    repository

--- test/services/EntryServiceTest.java
+++ test/services/EntryServiceTest.java
@@ -4,8 +4,8 @@ import datas.models.Entry;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import services.dtos.EntryCreationRequest;
-import services.dtos.EntryUpdateRequest;
+import dtos.EntryCreationRequest;
+import dtos.EntryUpdateRequest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
