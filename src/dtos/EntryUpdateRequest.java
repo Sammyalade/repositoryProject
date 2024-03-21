5eea1eafa9d7ca2cb8a37b2495e2329bb946c74f
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-21 14:13:15 +0100

    repository

--- src/dtos/EntryUpdateRequest.java
+++ src/dtos/EntryUpdateRequest.java
@@ -5,6 +5,15 @@ public class EntryUpdateRequest {
     private long id;
     private String title;
     private String body;
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
     public String getTitle() {
         return title;
